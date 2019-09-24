1ac108c233214c8dd5f6f35ab8717363d91dadad
Chudier <chudierp@gmail.com>
2019-09-24 16:48:00 -0700

    first commit for ability class

---
+++ superheroes.py
@@ -0,0 +1,17 @@
+import random
+random.randint(2,7)
+
+class Ability:
+    def __init__(self, name, attack_strength):
+        self.name = name
+        self.attack_strength = attack_strength
+
+    def attack(self):
+        return random.randint(0,self.attack_strength)
+
+
+
+if __name__ == "__main__":
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
