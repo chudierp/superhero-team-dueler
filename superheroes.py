c1d623663054e54d936048ce2301cf23023c6696
Chudier <chudierp@gmail.com>
2019-09-26 22:14:33 -0700

    page two complete and running

--- superheroes.py
+++ superheroes.py
@@ -23,30 +23,71 @@ class Hero:
         self.starting_health = starting_health
         self.current_health = starting_health 
         self.abilities = []
+        self.armors = []
 
     def add_ability(self, ability):
         self.abilities.append(ability)
 
     def attack(self):
         total_attack = 0
-        for abilities in self.abilities:
-            attack = abilities.attack()
-            total_attack = total_attack + attack
+        for ability in self.abilities:
+            attack = ability.attack()
+            total_attack += attack
             return total_attack
 
     def add_armor(self, armor):
-        self.armors = armor  
+        # self.armors = armor 
+        self.armors.append(armor) 
 
-    def defend(self, damage_amt):
-        self.damage_amt = damage_amt
+    def defend(self):
+        # self.damage_amt = damage_amt
+        if len(self.armors) == 0:
+            return 0
+
         for armor in self.armors:
             block = armor.block()
+            total_block = 0
             total_block = total_block + block
             return total_block
 
 
+    def take_damage(self, damage):
+        self.damage = damage
+        block_damage = self.defend()
+        damage -= block_damage
+        self.current_health -= damage
+        # return self 
 
+    def is_alive(self):
+        if self.current_health > 0: 
+            return True
+        return False    
 
+    def fight(self, opponent):   
+        while (self.is_alive()== True and opponent.is_alive() == True):
+            opponent.take_damage(self.attack())
+            self.take_damage(opponent.attack())
+
+            if opponent.current_health < self.attack():
+               opponent.is_alive() == False
+               print(self.name + " won! ")
+            elif self.current_health < opponent.attack():
+                self.is_alive() == False
+                print(opponent.name + " won! ")   
+
+        # if len(self.abilities) == 0:
+        #     print("Draw")  
+                
+        # # while opponent.current_health > 0:
+        #     while opponent.take_damage(self.attack):
+        #         if opponent.current_health < self.attack:
+        #             return self.current_health > 0
+        #                 print(self.name + " won! ") 
+                    
+        #         # if 
+                     
+
+  
 
 if __name__ == "__main__":
     # ability = Ability("Debugging Ability", 20)
@@ -55,13 +96,34 @@ if __name__ == "__main__":
     # my_hero = Hero("Grace Hopper", 200)
     # print(my_hero.name)
     # print(my_hero.current_health)
-    ability = Ability("Great Debugging", 50)
-    another_ability = Ability("Smarty Pants", 90)
-    hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    hero.add_ability(another_ability)
-    print(hero.attack())
+    # ability = Ability("Great Debugging", 50)
+    # another_ability = Ability("Smarty Pants", 90)
+    # hero = Hero("Grace Hopper", 200)
+    # hero.take_damage(150)
+    # print(hero.is_alive())
+    # hero.take_damage(15000)
+    # print(hero.is_alive())
+    # shield = Armor("Shield", 50)
+    # hero.add_armor(shield)
+    # hero.take_damage(50)
+    # print(hero.current_health)
+    # hero.add_ability(ability)
+    # hero.add_ability(another_ability)
+    # print(hero.attack())
     # print(hero.abilities)
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
+
+
 
     
     
